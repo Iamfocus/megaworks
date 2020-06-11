54b448fa04cd05850debe4d5a567246a077f2f21
chrisdesmond3d <44975699+chrisdesmond3d@users.noreply.github.com>
2020-06-11 22:21:19 +0100

    megaworks

---
+++ js/prime.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $(".scroll").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
+
+  
+//image filter 
+const listItems = document.querySelectorAll('.main li');
+const allimages = document.querySelectorAll('.main .container-fluid .images');
+ 
+function toggleActiveClass(active){
+    listItems.forEach(item => {
+      item.classList.remove('active');
+    })
+    active.classList.add('active');
+}
+ 
+function toggleimages(dataClass){
+    if(dataClass === 'all'){
+        for(let i = 0; i<allimages.length; i++){
+            allimages[i].style.display = 'block';
+        }
+    }else{
+        for(let i = 0; i<allimages.length; i++)
+            allimages[i].dataset.class === dataClass ? allimages[i].style.display = 'block' : allimages[i].style.display = 'none';
+    }
+}
+ 
+for(let i = 0; i < listItems.length; i++){
+    listItems[i].addEventListener('click', function(){
+        toggleActiveClass(listItems[i]);
+        toggleimages(listItems[i].dataset.class);
+    });
+}
